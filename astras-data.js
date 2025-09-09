995589bdd08882f5a083bd632530a91fc61ac9ce
NHZallen <130979060+NHZallen@users.noreply.github.com>
2025-09-09 22:27:11 +0800

    Update astras-data.js
    更新

--- astras-data.js
+++ astras-data.js
@@ -4,6 +4,7 @@ const OFFICIAL_ASTRAS = [
         name: '旅遊小編',
         description: '這是一款專為社群平台設計的Astras，能以優雅且富有感染力的語言撰寫旅遊景點與飯店介紹，透過細膩的文筆展現高級質感，同時結合城市歷史背景與熱門主題標籤，吸引目標客群並提升內容曝光度',
         instructions: '撰寫一篇以高檔旅遊社群編輯口吻的旅遊文章...',
-        avatarUrl: "https://i.ibb.co/XXXXX/imagename.png"
+        avatarUrl: "<a href="https://ibb.co/v4WN9033"><img src="https://i.ibb.co/0yL05HCC/Chat-GPT-Image-2025-9-9-10-07-20.png" alt="Chat-GPT-Image-2025-9-9-10-07-20" border="0"></a>"
     },
 ];
+
