c5886991e5d4eec780c60b76dd599ad08b756d08
NHZallen <130979060+NHZallen@users.noreply.github.com>
2025-09-09 22:55:37 +0800

    Update astras-data.js
    更新

--- astras-data.js
+++ astras-data.js
@@ -4,7 +4,8 @@ const OFFICIAL_ASTRAS = [
         name: '旅遊小編',
         description: '這是一款專為社群平台設計的Astras，能以優雅且富有感染力的語言撰寫旅遊景點與飯店介紹...',
         instructions: '撰寫一篇以高檔旅遊社群編輯口吻的旅遊文章...',
-        avatarUrl: "https://github.com/NHZallen/astranos-chatbot/blob/main/assets/images/ChatGPT%20Image%202025%E5%B9%B49%E6%9C%889%E6%97%A5%20%E4%B8%8B%E5%8D%8810_07_20.png"
+        avatarUrl: null
     },
 ];
+
 
