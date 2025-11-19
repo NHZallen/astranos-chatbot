0af3a4e5e994fba9baa102253e3c83716df86799
NHZallen <130979060+NHZallen@users.noreply.github.com>
2025-11-19 18:50:08 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 ﻿// 【重要】每次更新 PWA 時，請手動增加此版本號！
-const CACHE_NAME = 'astra-chat-cache-v6';
+const CACHE_NAME = 'astra-chat-cache-v7';
 
 
 // 靜態資源，可以長期快取
@@ -8,7 +8,6 @@ const STATIC_ASSETS = [
   '/manifest.json',
   '/icon-192.png',
   '/icon-512.png',
-  '/i18n.js',
   '/demo-conversations.js',
   '/astras-data.js'
 ];
@@ -87,4 +86,5 @@ self.addEventListener('fetch', event => {
       return response || fetch(event.request);
     })
   );
+
 });
